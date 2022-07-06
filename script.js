579e5c274e462b5fd6d1bb52a65e4568a770d474
Mikhail-H <mikhailgorlach@gmail.com>
2022-07-06 23:38:13 +0300

    FIX: close web app logic

--- script.js
+++ script.js
@@ -266,7 +266,7 @@ function showWebAppMainButton(text) {
 
 Telegram.WebApp.onEvent('mainButtonClicked', () => {
   if (cart.isVisible) {
-    Telegram.WebApp.sendData('TEST data')
+    Telegram.WebApp.close()
   } else {
     cart.isVisible = true
     drawCart()
